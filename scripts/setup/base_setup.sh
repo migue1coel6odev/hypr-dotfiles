9855c2ddcd4098def7f21fa949ece512022e8ce9
Miguel Coelho <miguelcoelhodev@gmail.com>
2024-10-08 21:41:30 +0100

    added numlock hook to mkinitcpio

--- scripts/setup/base_setup.sh
+++ scripts/setup/base_setup.sh
@@ -2,5 +2,8 @@ sudo pacman-key --init
 sudo pacman-key --populate
 sudo timedatectl set-local-rtc 1
 sudo pacman -Sy
-sudo pacman -S --needed xdg-user-dirs ttf-liberation
+sudo pacman -S --needed xdg-user-dirs ttf-liberation 
 xdg-user-dirs-update
+git clone https://aur.archlinux.org/mkinitcpio-numlock.git ~/Apps/numlock
+cd numlock
+makepkg -i
