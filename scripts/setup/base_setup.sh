a3aa0feaf43834f0adaa2204ef7e73409bc58f1e
Miguel Coelho <miguelcoelhodev@gmail.com>
2024-10-10 19:32:38 +0100

    added jetbrains-mono

--- scripts/setup/base_setup.sh
+++ scripts/setup/base_setup.sh
@@ -2,7 +2,7 @@ sudo pacman-key --init
 sudo pacman-key --populate
 sudo timedatectl set-local-rtc 1
 sudo pacman -Sy
-sudo pacman -S --needed xdg-user-dirs ttf-liberation ttf-fira-code
+sudo pacman -S --needed xdg-user-dirs ttf-liberation ttf-fira-code ttf-jetbrains-mono
 xdg-user-dirs-update
 git clone https://aur.archlinux.org/mkinitcpio-numlock.git ~/Apps/numlock
 cd numlock
